44ca75e114374d37300839d96cf69d374c55b62d
(Brett Theroux) bretttheroux@gmail.com <bretttheroux@gmail.com>
2017-05-23 18:57:27 +0200

    first chunk done - now start on booking controller

---
+++ app/assets/javascripts/init_datepicker.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('.datepicker').datepicker({
+    weekStart: 1,
+    autoclose: true
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  resources :bikes, only: [:index, :show, :new, :create]
+  resources :bikes, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:create]
+  end
 
   resources :brand_models, only: [:index]
 
