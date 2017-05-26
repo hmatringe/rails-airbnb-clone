8854e1757ade91174b1f8c4065e2c7c485dbc4fb
Matringe <hadrien.matringe@gmail.com>
2017-05-26 11:23:07 +0200

    dashboard full plus plenty of details. This commit is far too big I konw

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524135555) do
+ActiveRecord::Schema.define(version: 20170525145106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20170524135555) do
     t.integer  "bike_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
     t.index ["bike_id"], name: "index_bookings_on_bike_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
